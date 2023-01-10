2c98fc111d1f13e7c4d8e1a198dbbff08edd3754
BOO720 <ochaeta56@gmail.com>
2023-01-09 17:53:52 -0800

    brandonoprojectbranch

--- models/index.js
+++ models/index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql')
 
 app.use(express.urlencoded({extended: false}));
+
+app.use(express.static('./public'))
+
+const router = require('.routes/user.js')
+
+app.use(router)
 
 app.get('/', (req, res) =>{
     res.send("Welcome to our homepage.")
